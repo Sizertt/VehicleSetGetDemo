9bc041f4711866c2eb2da262ccfabd544264485f
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-12-12 18:18:27 +0300

    VehicleSetGetDemo

---
+++ src/VehicleSetGetMethod.java
@@ -0,0 +1,10 @@
+class VehicleGetSetMethod {
+    public static void main(String[] args) {
+        Vehicle ferrari = new Vehicle(2, -2, 360, 12);
+        System.out.println("Макс скорость: "  + ferrari.getMaxSpeed() + "км/ч");
+        System.out.println("Число колёс: " + ferrari.getWheels());
+
+        ferrari.setWheels (4) ;
+        System.out.println("Число колёс(повторно)" + ferrari.getWheels());
+    }
+}
